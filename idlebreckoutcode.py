265cbea47607dbcb7b8226f69842353b92f24999
brick932 <97676199+brick932@users.noreply.github.com>
2022-01-13 07:34:12 -0500

    Add files via upload

---
+++ idlebreckoutcode.py
@@ -0,0 +1,7 @@
+Idle Breakout Save Code Generator by Jackson Fortanbary
+What level do you want to be at?
+9877776543236
+How much money do you want
+** Process Stopped **
+
+Press Enter to exit terminal
